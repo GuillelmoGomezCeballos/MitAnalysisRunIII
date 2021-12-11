0d3b7ac19fbe3159e5554ae2b7809823a26cb315
GuillelmoGomezCeballos <ceballos@cern.ch>
2021-12-11 10:16:02 -0500

    new

--- rdf/skimming/check_missing_skim_files.py
+++ rdf/skimming/check_missing_skim_files.py
@@ -2,7 +2,7 @@ import os, sys, getopt, time
 
 if __name__ == "__main__":
 
-    outputDir = "/mnt/T3_US_MIT/hadoop/scratch/ceballos/nanoaod/Skims"
+    outputDir = "/mnt/T3_US_MIT/hadoop/scratch/ceballos/nanoaod/skims_submit"
     outputForCondorCfg = "skim_input_condor_jobs.cfg"
     missingFilesCfg = "skim_input_condor_missing_jobs.cfg"
 
@@ -30,23 +30,50 @@ if __name__ == "__main__":
 
     missingFile = open(missingFilesCfg, 'w')
 
-    countSamples = 0
+    countMissingFiles = [0, 0, 0, 0]
     outputFile = open(outputForCondorCfg, 'r')
     while True:
         lineRaw = outputFile.readline()
         line = lineRaw.strip().split(None, 4)
         if not line:
             break
-        fileName1 = os.path.join(outputDir, "1l", line[3], "output_1l_{0}.root".format(line[1]))
-        fileName2 = os.path.join(outputDir, "2l", line[3], "output_2l_{0}.root".format(line[1]))
-        fileName3 = os.path.join(outputDir, "3l", line[3], "output_3l_{0}.root".format(line[1]))
-	isMissingFile = False
-	if  (not(os.path.exists(fileName1) and os.path.getsize(fileName1) > 0)): isMissingFile = True
-	elif(not(os.path.exists(fileName2) and os.path.getsize(fileName2) > 0)): isMissingFile = True
-	elif(not(os.path.exists(fileName3) and os.path.getsize(fileName3) > 0)): isMissingFile = True
-        if(isMissingFile == True):
+
+        isMissingFile = [True, True, True]
+        # 1l
+        fileNameA = os.path.join(outputDir, "1l", line[3], "output_1l_{0}.root".format(line[1]))
+        fileNameB = os.path.join(outputDir, "1l", line[3], "output_1l_{0}_{1}.root".format(line[0],line[1]))
+
+        if((os.path.exists(fileNameA) and os.path.getsize(fileNameA) > 1000) or
+           (os.path.exists(fileNameB) and os.path.getsize(fileNameB) > 1000)):
+            isMissingFile[0] = False
+        else:
+            countMissingFiles[0] += 1
+
+        # 2l
+        fileNameA = os.path.join(outputDir, "2l", line[3], "output_2l_{0}.root".format(line[1]))
+        fileNameB = os.path.join(outputDir, "2l", line[3], "output_2l_{0}_{1}.root".format(line[0],line[1]))
+
+        if((os.path.exists(fileNameA) and os.path.getsize(fileNameA) > 1000) or
+           (os.path.exists(fileNameB) and os.path.getsize(fileNameB) > 1000)):
+            isMissingFile[1] = False
+        else:
+            countMissingFiles[1] += 1
+
+        # 3l
+        fileNameA = os.path.join(outputDir, "3l", line[3], "output_3l_{0}.root".format(line[1]))
+        fileNameB = os.path.join(outputDir, "3l", line[3], "output_3l_{0}_{1}.root".format(line[0],line[1]))
+
+        if((os.path.exists(fileNameA) and os.path.getsize(fileNameA) > 1000) or
+           (os.path.exists(fileNameB) and os.path.getsize(fileNameB) > 1000)):
+            isMissingFile[2] = False
+        else:
+            countMissingFiles[2] += 1
+
+        if(isMissingFile[0] == True or isMissingFile[1] == True or isMissingFile[2] == True):
             missingFile.writelines(lineRaw)
+            countMissingFiles[3] += 1
 
     missingFile.close()
 
+    print("missingFiles: {0} / {1} / {2} / {3}".format(countMissingFiles[0],countMissingFiles[1],countMissingFiles[2],countMissingFiles[3]))
     os.system("wc {0};wc {1}".format(outputForCondorCfg,missingFilesCfg))
