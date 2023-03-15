0a46ecd2afd04f1c7a6462fec75c78870ffb8fb3
GuillelmoGomezCeballos <ceballos@cern.ch>
2023-03-15 04:30:28 -0400

    new

--- rdf/macros/analysis_slurm.sh
+++ rdf/macros/analysis_slurm.sh
@@ -8,7 +8,7 @@ time python3 $5.py --process=$1 --year=$2 --whichJob=$3
 status=$?
 
 if [ -f "fillhisto_$5_sample$1_year$2_job$3.root" ]; then
-  mv fillhisto_$5_sample$1_year$2_job$3.root fillhisto$5$4_sample$1_year$2_job$3.root
+  mv fillhisto_$5_sample$1_year$2_job$3.root fillhisto_$5$4_sample$1_year$2_job$3.root
   echo "DONE"
 
 elif [ $status -eq 0 ]; then
