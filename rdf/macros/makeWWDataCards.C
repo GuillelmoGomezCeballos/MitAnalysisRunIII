a694238b36530a86b5d6a587ad83e014f44222c7
GuillelmoGomezCeballos <ceballos@cern.ch>
2023-06-01 17:33:53 -0400

    new

---
+++ rdf/macros/makeWWDataCards.C
@@ -0,0 +1,442 @@
+#include <TROOT.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TSystem.h>
+#include <TString.h>
+#include <TRandom.h>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TMath.h>
+#include <iostream>
+#include <fstream>
+#include "TLorentzVector.h"
+#include "TColor.h"
+
+#include "../makePlots/common.h"
+
+void makeWWDataCards(int fidAna = 1, TString InputDir = "anaZ", TString anaSel = "wwAnalysis1005", int year = 2022){
+  TFile *inputFile;
+  TFile *outputFile;
+  const int nSelTotal = 4;
+  const int nSystTotal = 141;
+
+  TH1D *histo_Auxiliar[nSelTotal][nPlotCategories];
+  TH1D *histo_Baseline[nPlotCategories];
+  TH1D *histo_QCDScaleUp[nPlotCategories];
+  TH1D *histo_QCDScaleDown[nPlotCategories];
+  TH1D *histo_PSUp[nPlotCategories];
+  TH1D *histo_PSDown[nPlotCategories];
+
+  for(int ic=0; ic<nPlotCategories; ic++) {
+    histo_Baseline[ic] = new TH1D(Form("histo_%s",plotBaseNames[ic].Data()),Form("histo_%s",plotBaseNames[ic].Data()), 4, -0.5, 3.5);
+    TString plotBaseNamesTemp =  plotBaseNames[ic];
+    if(ic == kPlotSignal0 || ic == kPlotSignal1 ||
+       ic == kPlotSignal2 || ic == kPlotSignal3) plotBaseNamesTemp = "WW";
+    histo_QCDScaleUp  [ic] = (TH1D*)histo_Baseline[ic]->Clone(Form("histo_%s_QCDScale_%s_ACCEPTUp"   , plotBaseNames[ic].Data(), plotBaseNamesTemp.Data()));
+    histo_QCDScaleDown[ic] = (TH1D*)histo_Baseline[ic]->Clone(Form("histo_%s_QCDScale_%s_ACCEPTDown" , plotBaseNames[ic].Data(), plotBaseNamesTemp.Data()));
+    histo_PSUp  [ic]       = (TH1D*)histo_Baseline[ic]->Clone(Form("histo_%s_PS_%s_ACCEPTUp"   , plotBaseNames[ic].Data(), plotBaseNamesTemp.Data()));
+    histo_PSDown[ic]       = (TH1D*)histo_Baseline[ic]->Clone(Form("histo_%s_PS_%s_ACCEPTDown" , plotBaseNames[ic].Data(), plotBaseNamesTemp.Data()));
+  }
+
+  TString nameSyst[nSystTotal];
+  nameSyst[ 0] = "BtagSFBC_correlatedUp";
+  nameSyst[ 1] = "BtagSFBC_correlatedDown";
+  nameSyst[ 2] = "BtagSFBC_uncorrelatedUp";
+  nameSyst[ 3] = "BtagSFBC_uncorrelatedDown";
+  nameSyst[ 4] = "BtagSFLF_correlatedUp";
+  nameSyst[ 5] = "BtagSFLF_correlatedDown";
+  nameSyst[ 6] = "BtagSFLF_uncorrelatedUp";
+  nameSyst[ 7] = "BtagSFLF_uncorrelatedDown";
+  nameSyst[ 8] = "MuoSFTRKUp";
+  nameSyst[ 9] = "MuoSFTRKDown";
+  nameSyst[10] = "MuoSFIDUp";
+  nameSyst[11] = "MuoSFIDDown";
+  nameSyst[12] = "MuoSFISOUp";
+  nameSyst[13] = "MuoSFISODown";
+  nameSyst[14] = "EleSFTRKUp";
+  nameSyst[15] = "EleSFTRKDown";
+  nameSyst[16] = "EleSFIDUp";
+  nameSyst[17] = "EleSFIDDown";
+  nameSyst[18] = "PUSFUp";
+  nameSyst[19] = "PUSFDown";
+  nameSyst[20] = "PS0";
+  nameSyst[21] = "PS1";
+  nameSyst[22] = "PS2";
+  nameSyst[23] = "PS3";
+  nameSyst[24] = "QCDScale0";
+  nameSyst[25] = "QCDScale1";
+  nameSyst[26] = "QCDScale2";
+  nameSyst[27] = "QCDScale3";
+  nameSyst[28] = "QCDScale4";
+  nameSyst[29] = "QCDScale5";
+  for(int i=0; i<=102; i++) nameSyst[30+i] = Form("PDF%d",i);
+  nameSyst[133] = "JesUp";
+  nameSyst[134] = "JesDown";
+  nameSyst[135] = "JerUp";
+  nameSyst[136] = "JerDown";
+  nameSyst[137] = "MuonMomUp";
+  nameSyst[138] = "MuonMomDown";
+  nameSyst[139] = "ElectronMomUp";
+  nameSyst[140] = "ElectronMomDown";
+
+  TH1D *histo_Syst[nSystTotal][nPlotCategories];
+  for(int j=0; j<nSystTotal; j++){
+    for(int ic=0; ic<nPlotCategories; ic++) histo_Syst[j][ic] = new TH1D(Form("histo_%s_%s",plotBaseNames[ic].Data(),nameSyst[j].Data()),Form("histo_%s_%s",plotBaseNames[ic].Data(),nameSyst[j].Data()), 4, -0.5, 3.5);
+  }
+
+  // same-sign / WW / DY / Top
+  for(unsigned nSel=0; nSel<nSelTotal; nSel++) {
+    inputFile = new TFile(Form("%s/fillhisto_%s_%d_%d_mva.root",InputDir.Data(),anaSel.Data(),year,nSel*150), "read");
+    for(unsigned ic=kPlotData; ic!=nPlotCategories; ic++) {
+      histo_Auxiliar[nSel][ic] = (TH1D*)inputFile->Get(Form("histoMVA%d", ic)); assert(histo_Auxiliar[nSel][ic]);
+      histo_Baseline[ic]->SetBinContent(nSel+1,histo_Auxiliar[nSel][ic]->GetBinContent(fidAna));
+      histo_Baseline[ic]->SetBinError  (nSel+1,histo_Auxiliar[nSel][ic]->GetBinError  (fidAna));
+    }
+    delete inputFile;
+  }
+
+  for(int j=0; j<nSystTotal; j++){
+    for(unsigned nSel=0; nSel<nSelTotal; nSel++) {
+      inputFile = new TFile(Form("%s/fillhisto_%s_%d_%d_mva.root",InputDir.Data(),anaSel.Data(),year,nSel*150+1+j), "read");
+      for(unsigned ic=kPlotData; ic!=nPlotCategories; ic++) {
+        histo_Auxiliar[nSel][ic] = (TH1D*)inputFile->Get(Form("histoMVA%d", ic)); assert(histo_Auxiliar[nSel][ic]);
+        histo_Syst[j][ic]->SetBinContent(nSel+1,histo_Auxiliar[nSel][ic]->GetBinContent(fidAna));
+        histo_Syst[j][ic]->SetBinError  (nSel+1,histo_Auxiliar[nSel][ic]->GetBinError  (fidAna));
+      }
+      delete inputFile;
+    }
+  }
+
+  for(unsigned ic=0; ic<nPlotCategories; ic++) {
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    for(int nb=1; nb<=histo_Baseline[ic]->GetNbinsX(); nb++){
+      histo_Baseline[ic]->SetBinContent(nb, TMath::Max((float)histo_Baseline[ic]->GetBinContent(nb),0.0f));
+
+      // compute QCD scale uncertainties bin-by-bin
+      double diffQCDScale[6] = {
+       TMath::Abs(histo_Syst[24][ic]->GetBinContent(nb)-histo_Baseline[ic]->GetBinContent(nb)),
+       TMath::Abs(histo_Syst[25][ic]->GetBinContent(nb)-histo_Baseline[ic]->GetBinContent(nb)),
+       TMath::Abs(histo_Syst[26][ic]->GetBinContent(nb)-histo_Baseline[ic]->GetBinContent(nb)),
+       TMath::Abs(histo_Syst[27][ic]->GetBinContent(nb)-histo_Baseline[ic]->GetBinContent(nb)),
+       TMath::Abs(histo_Syst[28][ic]->GetBinContent(nb)-histo_Baseline[ic]->GetBinContent(nb)),
+       TMath::Abs(histo_Syst[29][ic]->GetBinContent(nb)-histo_Baseline[ic]->GetBinContent(nb))};
+
+      double systQCDScale = diffQCDScale[0];
+      for(int nqcd=1; nqcd<6; nqcd++) {
+        if(diffQCDScale[nqcd] > systQCDScale) systQCDScale = diffQCDScale[nqcd];
+      }
+
+      if(histo_Baseline[ic]->GetBinContent(nb) > 0) 
+        systQCDScale = 1.0+systQCDScale/histo_Baseline[ic]->GetBinContent(nb);
+      else systQCDScale = 1;
+
+      histo_QCDScaleUp  [ic]->SetBinContent(nb, histo_Baseline[ic]->GetBinContent(nb)*systQCDScale);
+      histo_QCDScaleDown[ic]->SetBinContent(nb, histo_Baseline[ic]->GetBinContent(nb)/systQCDScale);
+
+      // compute PS uncertainties bin-by-bin
+      double diffPS[4] = {
+       TMath::Abs(histo_Syst[20][ic]->GetBinContent(nb)-histo_Baseline[ic]->GetBinContent(nb)),
+       TMath::Abs(histo_Syst[21][ic]->GetBinContent(nb)-histo_Baseline[ic]->GetBinContent(nb)),
+       TMath::Abs(histo_Syst[22][ic]->GetBinContent(nb)-histo_Baseline[ic]->GetBinContent(nb)),
+       TMath::Abs(histo_Syst[23][ic]->GetBinContent(nb)-histo_Baseline[ic]->GetBinContent(nb))};
+
+      double systPS = diffPS[0];
+      for(int nps=1; nps<4; nps++) {
+        if(diffPS[nps] > systPS) systPS = diffPS[nps];
+      }
+
+      if(histo_Baseline[ic]->GetBinContent(nb) > 0) 
+        systPS = 1.0+systPS/histo_Baseline[ic]->GetBinContent(nb);
+      else systPS = 1;
+
+      histo_PSUp  [ic]->SetBinContent(nb, histo_Baseline[ic]->GetBinContent(nb)*systPS);
+      histo_PSDown[ic]->SetBinContent(nb, histo_Baseline[ic]->GetBinContent(nb)/systPS);
+
+      histo_Baseline    [ic]->SetBinContent(nb, TMath::Max((float)histo_Baseline    [ic]->GetBinContent(nb),0.0f));
+      histo_QCDScaleUp  [ic]->SetBinContent(nb, TMath::Max((float)histo_QCDScaleUp  [ic]->GetBinContent(nb),0.0f));
+      histo_QCDScaleDown[ic]->SetBinContent(nb, TMath::Max((float)histo_QCDScaleDown[ic]->GetBinContent(nb),0.0f));
+      histo_PSUp        [ic]->SetBinContent(nb, TMath::Max((float)histo_PSUp        [ic]->GetBinContent(nb),0.0f));
+      histo_PSDown      [ic]->SetBinContent(nb, TMath::Max((float)histo_PSDown      [ic]->GetBinContent(nb),0.0f));
+      for(int j=0; j<nSystTotal; j++) histo_Syst[j][ic]->SetBinContent(nb, TMath::Max((float)histo_Syst[j][ic]->GetBinContent(nb),0.0f));
+    }
+  }
+
+  TString outputLimits = Form("output_%s_%d_bin%d.root",anaSel.Data(),year,fidAna);
+  outputFile = new TFile(outputLimits, "RECREATE");
+  outputFile->cd();
+  for(unsigned ic=kPlotData; ic!=nPlotCategories; ic++) {
+    if(histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    histo_Baseline[ic]->Write();
+    for(int j=0; j<nSystTotal; j++) histo_Syst[j][ic]->Write();
+
+    if(ic == kPlotqqWW || ic == kPlotggWW || ic == kPlotSignal0 || ic == kPlotSignal1 || ic == kPlotSignal2 || ic == kPlotSignal3) {
+      histo_QCDScaleUp  [ic]->Scale(histo_Baseline[ic]->GetSumOfWeights()/histo_QCDScaleUp  [ic]->GetSumOfWeights());
+      histo_QCDScaleDown[ic]->Scale(histo_Baseline[ic]->GetSumOfWeights()/histo_QCDScaleDown[ic]->GetSumOfWeights());
+      histo_PSUp        [ic]->Scale(histo_Baseline[ic]->GetSumOfWeights()/histo_PSUp        [ic]->GetSumOfWeights());
+      histo_PSDown      [ic]->Scale(histo_Baseline[ic]->GetSumOfWeights()/histo_PSDown      [ic]->GetSumOfWeights());
+    }
+    histo_QCDScaleUp  [ic]->Write();
+    histo_QCDScaleDown[ic]->Write();
+    histo_PSUp        [ic]->Write();
+    histo_PSDown      [ic]->Write();
+  }
+  outputFile->Close();
+
+  // Filling datacards txt file
+  char outputLimitsCard[200];  					  
+  sprintf(outputLimitsCard,"datacard_%s_%d_bin%d.txt",anaSel.Data(),year,fidAna);
+  ofstream newcardShape;
+  newcardShape.open(outputLimitsCard);
+  newcardShape << Form("imax * number of channels\n");
+  newcardShape << Form("jmax * number of background minus 1\n");
+  newcardShape << Form("kmax * number of nuisance parameters\n");
+
+  newcardShape << Form("shapes    *   *   %s  histo_$PROCESS histo_$PROCESS_$SYSTEMATIC\n",outputLimits.Data());
+  newcardShape << Form("shapes data_obs * %s  histo_Data\n",outputLimits.Data());
+
+  newcardShape << Form("Observation %f\n",histo_Baseline[kPlotData]->GetSumOfWeights());
+
+  newcardShape << Form("bin   ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    newcardShape << Form("ch%d ",fidAna);
+
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("process  ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    newcardShape << Form("%s  ", plotBaseNames[ic].Data());
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("process  ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if     (ic != kPlotSignal0 &&
+            ic != kPlotSignal1 &&
+            ic != kPlotSignal2 &&
+            ic != kPlotSignal3
+            ) newcardShape << Form("%d  ", ic);
+    else if(ic == kPlotSignal0) newcardShape << Form("%d  ", -1);
+    else if(ic == kPlotSignal1) newcardShape << Form("%d  ", -2);
+    else if(ic == kPlotSignal2) newcardShape << Form("%d  ", -3);
+    else if(ic == kPlotSignal3) newcardShape << Form("%d  ", -4);
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("rate  ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    newcardShape << Form("%f  ", histo_Baseline[ic]->GetSumOfWeights());
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("CMS_fake_norn	 lnN	 ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("%6.3f ",1.20);
+    else		     newcardShape << Form("- "); 
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("CMS_lumi_%d   lnN     ",year);
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("%6.3f ",1.02);
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("BtagSFBC_correlated shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("BtagSFBC_uncorrelated shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("BtagSFLF_correlated shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("BtagSFLF_uncorrelated shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("MuoSFTRK shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("MuoSFID shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("MuoSFISO shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("EleSFTRK shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+ 
+  newcardShape << Form("EleSFID shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("PUSF shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		 newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+ 
+  newcardShape << Form("Jes shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+ 
+  //newcardShape << Form("Jer shape ");
+  //for (int ic=0; ic<nPlotCategories; ic++){
+  //  if(!histo_Baseline[ic]) continue;
+  //  if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+  //  if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+  //  else		       newcardShape << Form("1.0 ");
+  //}
+  //newcardShape << Form("\n");
+ 
+  newcardShape << Form("MuonMom shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+ 
+  newcardShape << Form("ElectronMom shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic == kPlotNonPrompt) newcardShape << Form("- ");
+    else		     newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+
+  for(unsigned ic=0; ic<nPlotCategories; ic++) {
+    if(ic== kPlotData || ic == kPlotNonPrompt || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic== kPlotSignal0 || ic == kPlotSignal1 || ic== kPlotSignal2 || ic == kPlotSignal3) continue;
+    newcardShape << Form("QCDScale_%s_ACCEPT	shape	",plotBaseNames[ic].Data());
+    for(unsigned ic2=0; ic2<nPlotCategories; ic2++) {
+      if(ic2 == kPlotData || histo_Baseline[ic2]->GetSumOfWeights() <= 0) continue;
+  	  if(ic==ic2) newcardShape << Form("1.0  ");
+  	  else        newcardShape << Form("-  ");
+      }
+      newcardShape << Form("\n");
+  } 
+
+  newcardShape << Form("QCDScale_WW_ACCEPT shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if     (ic != kPlotSignal0 &&
+            ic != kPlotSignal1 &&
+            ic != kPlotSignal2 &&
+            ic != kPlotSignal3
+            ) newcardShape << Form("- ");
+    else      newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+
+  for(unsigned ic=0; ic<nPlotCategories; ic++) {
+    if(ic== kPlotData || ic == kPlotNonPrompt || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if(ic== kPlotSignal0 || ic == kPlotSignal1 || ic== kPlotSignal2 || ic == kPlotSignal3) continue;
+    newcardShape << Form("PS_%s_ACCEPT	shape	",plotBaseNames[ic].Data());
+    for(unsigned ic2=0; ic2<nPlotCategories; ic2++) {
+      if(ic2 == kPlotData || histo_Baseline[ic2]->GetSumOfWeights() <= 0) continue;
+  	  if(ic==ic2) newcardShape << Form("1.0  ");
+  	  else        newcardShape << Form("-  ");
+      }
+      newcardShape << Form("\n");
+  } 
+
+  newcardShape << Form("PS_WW_ACCEPT shape ");
+  for (int ic=0; ic<nPlotCategories; ic++){
+    if(!histo_Baseline[ic]) continue;
+    if(ic == kPlotData || histo_Baseline[ic]->GetSumOfWeights() <= 0) continue;
+    if     (ic != kPlotSignal0 &&
+            ic != kPlotSignal1 &&
+            ic != kPlotSignal2 &&
+            ic != kPlotSignal3
+            ) newcardShape << Form("- ");
+    else      newcardShape << Form("1.0 ");
+  }
+  newcardShape << Form("\n");
+
+  newcardShape << Form("CMS_ww_fakenorm_bin%d_%d  rateParam * %s 1 [0.1,3.9]\n",fidAna,year,plotBaseNames[kPlotNonPrompt].Data());
+  newcardShape << Form("CMS_ww_dytautaunorm_bin%d_%d  rateParam * %s 1 [0.1,1.9]\n",fidAna,year,plotBaseNames[kPlotDY].Data());
+  newcardShape << Form("CMS_ww_topnorm_bin%d_%d  rateParam * %s 1 [0.1,1.9]\n",fidAna,year,plotBaseNames[kPlotTop].Data());
+
+  newcardShape << Form("ch%d autoMCStats 0\n",fidAna);
+
+  newcardShape.close();
+
+}
