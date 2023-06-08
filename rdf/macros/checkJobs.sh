f6e29c8396c5cfe04651736deebc493e30d45d42
GuillelmoGomezCeballos <ceballos@cern.ch>
2023-06-08 11:14:16 -0400

    new

--- rdf/macros/checkJobs.sh
+++ rdf/macros/checkJobs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ $# -lt 3 ]; then
+if [ $# -lt 4 ]; then
    echo "TOO FEW PARAMETERS"
    exit
 fi
@@ -8,6 +8,7 @@ fi
 export theAna=$1
 export condorJob=$2
 export year=$3
+export queue=$4
 
 export group=10
 
@@ -19,10 +20,18 @@ elif [ $theAna = 'trigger' ]; then
 
 fi
 
+if [ $4 == 'slurm' ]; then
 ls logs/simple_${theAna}Analysis_${condorJob}_*_${year}_3_*.out|wc -l|awk '{printf("%d\n",$1*ENVIRON["group"])}'|awk -f ~/bin/sum2.awk;
 grep "Total files" logs/simple_${theAna}Analysis_${condorJob}_*_${year}_3_*.out|awk '{a=$3;if(a>ENVIRON["group"])a=ENVIRON["group"];printf("%d\n",a)}'|awk -f ~/bin/sum2.awk;
 grep "Total files" logs/simple_${theAna}Analysis_${condorJob}_*_${year}_5_*.out|awk '{a=$3;if(a>ENVIRON["group"])a=ENVIRON["group"];printf("%d\n",a)}'|awk -f ~/bin/sum2.awk;
 grep "Total files" logs/simple_${theAna}Analysis_${condorJob}_*_${year}_7_*.out|awk '{a=$3;if(a>ENVIRON["group"])a=ENVIRON["group"];printf("%d\n",a)}'|awk -f ~/bin/sum2.awk;
+else
+ls logs/simple_${theAna}Analysis_${condorJob}_*_${year}_3.out|wc -l|awk '{printf("%d\n",$1*ENVIRON["group"])}'|awk -f ~/bin/sum2.awk;
+grep "Total files" logs/simple_${theAna}Analysis_${condorJob}_*_${year}_3.out|awk '{a=$3;if(a>ENVIRON["group"])a=ENVIRON["group"];printf("%d\n",a)}'|awk -f ~/bin/sum2.awk;
+grep "Total files" logs/simple_${theAna}Analysis_${condorJob}_*_${year}_5.out|awk '{a=$3;if(a>ENVIRON["group"])a=ENVIRON["group"];printf("%d\n",a)}'|awk -f ~/bin/sum2.awk;
+grep "Total files" logs/simple_${theAna}Analysis_${condorJob}_*_${year}_7.out|awk '{a=$3;if(a>ENVIRON["group"])a=ENVIRON["group"];printf("%d\n",a)}'|awk -f ~/bin/sum2.awk;
+fi
+
 grep FAILED logs/simple_${theAna}Analysis_${condorJob}_*_${year}_*;
 grep DONE   logs/simple_${theAna}Analysis_${condorJob}_*_${year}_*|wc;
 grep DONE   logs/simple_${theAna}Analysis_${condorJob}_*_${year}_*|grep -v FILES|wc;
