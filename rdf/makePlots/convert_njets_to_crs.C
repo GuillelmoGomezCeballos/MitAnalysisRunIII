aaa994d648c5b2288059b66d45470889e8f50e23
GuillelmoGomezCeballos <ceballos@cern.ch>
2024-03-22 02:09:00 -0400

    new

--- rdf/makePlots/convert_njets_to_crs.C
+++ rdf/makePlots/convert_njets_to_crs.C
@@ -14,18 +14,22 @@ void convert_njets_to_crs(TString postfix = ""){
   }
 
   TH1F* histo[njets][nPlotCategories];
+  TH1F* histo_total[njets];
   for(int nj=0; nj<njets; nj++) for(int nc=0; nc<nPlotCategories; nc++) histo[nj][nc] = NULL;
 
   for(int nj=0; nj<njets; nj++) {
     for(int nc=0; nc<nPlotCategories; nc++) {
       histo[nj][nc] = (TH1F*)fileInput[nj]->Get(Form("histo%d",nc));
     }
+    histo_total[nj] = (TH1F*)fileInput[nj]->Get(Form("histo_total"));
   }
 
   TH1F* _hist[nPlotCategories];
+  TH1F* _histo_total;
   for(int nc=0; nc<nPlotCategories; nc++) {
     _hist[nc] = new TH1F(Form("histo%d",nc), Form("histo%d",nc), 4, -0.5, 3.5);
   }
+  _histo_total = new TH1F(Form("histo_total"), Form("histo_total"), 4, -0.5, 3.5);
 
   for(int ncr=0; ncr<5; ncr++){
     TFile* outFileLimits = new TFile(Form("ww_output_cr%d%s.root",ncr,postfix.Data()),"recreate");
@@ -45,6 +49,15 @@ void convert_njets_to_crs(TString postfix = ""){
       }
       if(isNonZero == true) _hist[nc]->Write();
     }
+
+    if(histo_total[0]){
+      for(int nj=0; nj<njets; nj++) {
+        _histo_total->SetBinContent(nj+1, histo_total[nj]->GetBinContent(ncr+1));
+        _histo_total->SetBinError  (nj+1, histo_total[nj]->GetBinError  (ncr+1));
+      }
+      _histo_total->Write();
+    }
+
     outFileLimits->Close();
   }
 }
